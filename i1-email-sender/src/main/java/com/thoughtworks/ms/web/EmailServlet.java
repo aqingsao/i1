37b3c0c1fd80b6867dc5948046174824fe6651b3
aqingsao <xqzhang@thoughtworks.com>
2013-01-31 16:19:31 +0800

    Make email sender a web app

---
+++ i1-email-sender/src/main/java/com/thoughtworks/ms/web/EmailServlet.java
@@ -0,0 +1,8 @@
+package com.thoughtworks.ms.web;
+
+import javax.inject.Singleton;
+import javax.servlet.http.HttpServlet;
+
+@Singleton
+public class EmailServlet extends HttpServlet {
+}
---
+++ i1-email-sender/src/main/java/com/thoughtworks/ms/web/MyGuiceServletContextListener.java
@@ -0,0 +1,37 @@
+package com.thoughtworks.ms.web;
+
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import com.google.inject.name.Names;
+import com.google.inject.servlet.GuiceServletContextListener;
+import com.google.inject.servlet.ServletModule;
+
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class MyGuiceServletContextListener extends GuiceServletContextListener {
+    private static final Logger LOGGER = Logger.getLogger(MyGuiceServletContextListener.class.getName());
+
+    @Override
+    protected Injector getInjector() {
+        return Guice.createInjector(new ServletModule() {
+            protected void configureServlets() {
+                Names.bindProperties(binder(), loadProperties());
+                serve("/upload").with(EmailServlet.class);
+            }
+        });
+    }
+
+    private Properties loadProperties() {
+        Properties properties = new Properties();
+
+        String propertyFile = "/app.properties";
+        try {
+            properties.load(getClass().getResourceAsStream(propertyFile));
+        } catch (Exception e) {
+            LOGGER.log(Level.WARNING, "Failed to load property file " + propertyFile);
+        }
+        return properties;
+    }
+}
