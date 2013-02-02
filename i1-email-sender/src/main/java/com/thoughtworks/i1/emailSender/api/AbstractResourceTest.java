f43e807fb76f0b79339007a390a2b804184d6258
aqingsao <xqzhang@thoughtworks.com>
2013-02-02 19:58:50 +0800

    EmailResourceTest could run successfully

---
+++ i1-email-sender/src/main/java/com/thoughtworks/i1/emailSender/api/AbstractResourceTest.java
@@ -0,0 +1,32 @@
+package com.thoughtworks.i1.emailSender.api;
+
+import com.sun.jersey.api.container.grizzly2.GrizzlyServerFactory;
+import com.sun.jersey.api.core.PackagesResourceConfig;
+import com.sun.jersey.api.core.ResourceConfig;
+import org.glassfish.grizzly.http.server.HttpServer;
+
+import javax.ws.rs.core.UriBuilder;
+import java.io.IOException;
+import java.net.URI;
+
+public abstract class AbstractResourceTest {
+    protected static final URI BASE_URI = baseURI();
+    protected HttpServer httpServer = createHttpServer();
+
+    private static URI baseURI() {
+        return UriBuilder.fromUri("http://localhost/").port(9998).build();
+    }
+
+    private HttpServer createHttpServer() {
+        ResourceConfig rc = new PackagesResourceConfig("com.thoughtworks.i1.emailSender.api");
+        try {
+            return GrizzlyServerFactory.createHttpServer(BASE_URI, rc);
+        } catch (IOException e) {
+            throw new RuntimeException(e.getMessage(), e);
+        }
+    }
+
+    protected URI uri(String path) {
+        return UriBuilder.fromUri(BASE_URI).path(path).build();
+    }
+}
--- i1-email-sender/src/test/java/com/thoughtworks/i1/emailSender/api/EmailResourceTest.java
+++ i1-email-sender/src/test/java/com/thoughtworks/i1/emailSender/api/EmailResourceTest.java
@@ -1,23 +1,45 @@
 package com.thoughtworks.i1.emailSender.api;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.WebResource;
-import org.junit.Ignore;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.UriBuilder;
+
+import java.io.IOException;
+import java.net.URI;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
-public class EmailResourceTest {
-    @Ignore("Not implemented yet")
-    public void test_send_email() {
-        String url = "http://localhost:8051/emailSender/api/email";
-        WebResource webResource = Client.create().resource(url);
-        ClientResponse response = webResource.accept(MediaType.APPLICATION_JSON_TYPE).post(ClientResponse.class);
+public class EmailResourceTest extends AbstractResourceTest {
+
+    @Before
+    public void before() throws IOException {
+        httpServer.start();
+    }
+
+    @After
+    public void after() {
+        httpServer.stop();
+    }
+
+    @Test
+    public void test_send_email() throws IOException {
+        WebResource webResource = Client.create().resource(uri("/email"));
+        ClientResponse response = webResource.accept(MediaType.APPLICATION_JSON_TYPE).get(ClientResponse.class);
 
         assertThat(response.getClientResponseStatus(), is(ClientResponse.Status.OK));
+        String entity = response.getEntity(String.class);
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode jsonNodes = mapper.readTree(entity);
+        assertThat(jsonNodes.has("subject"), is(true));
     }
+
 }
