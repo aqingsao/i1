bd70ee480f0da3b488877ffa63a818ff1e1d10e9
aqingsao <xqzhang@thoughtworks.com>
2013-02-27 22:47:18 +0800

    Introduce angularJS

---
+++ i1-email-sender/src/main/webapp/js/email.js
@@ -0,0 +1,19 @@
+function EmailsCtrl($scope) {
+    $scope.emails = [
+        {sender:'admin@i1.com', subject:'test', message:'message', recipients: "a@b.com, b@c.com", time: '2012-12-12', result: 'success'},
+        {sender:'admin@i1.com', subject:'test', message:'message', recipients: "a@b.com, b@c.com", time: '2012-12-12', result: 'warning'},
+        {sender:'admin@i1.com', subject:'test', message:'message', recipients: "a@b.com, b@c.com", time: '2012-12-12', result: 'error'}]
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
